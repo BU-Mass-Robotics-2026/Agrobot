1915c9da600942ed29d2578d66d1b6c5a870a8a4
buroboticsclub <bostonuniversityrobotics@gmail.com>
2026-04-16 21:03:18 -0400

    Added joint 0, end effector; updated moveit_config

--- install/epos2_bridge/share/epos2_bridge/launch/epos2_arm_controller.launch.py
+++ install/epos2_bridge/share/epos2_bridge/launch/epos2_arm_controller.launch.py
@@ -1,1 +1,15 @@
-/home/robotics-club/agrobot_ws/build/epos2_bridge/launch/epos2_arm_controller.launch.py
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="epos2_bridge",
+            executable="epos2_arm_controller",
+            name="epos2_arm_controller",
+            output="screen",
+            parameters=[
+                "/home/robotics-club/agrobot_ws/src/epos2_bridge/config/epos2_arm_controller.yaml"
+            ],
+        )
+    ])
